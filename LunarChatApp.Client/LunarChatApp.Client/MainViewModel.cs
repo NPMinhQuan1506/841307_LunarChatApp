5ef2c852ef892c138fff2e6369457bab1466743d
NP Minh Quan <npminhquan1506@gmail.com>
2022-10-08 13:08:36 +0700

    Setting structure project

---
+++ LunarChatApp.Client/LunarChatApp.Client/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LunarChatApp.Client
+{
+    [POCOViewModel()]
+    public class MainViewModel
+    {
+    }
+}
